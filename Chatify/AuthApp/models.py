86c677f42ff1d091a19c4d069f2624676cc2a5d1
alexborodach30 <sagopien04@gmail.com>
2025-03-12 20:27:39 +0300

    User model.

--- Chatify/AuthApp/models.py
+++ Chatify/AuthApp/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password):
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=128, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    google_id = models.CharField(max_length=255, blank=True, null=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = "users"
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+        indexes = [models.Index(["date_created"],
+                                name="date_created_index")]
+
